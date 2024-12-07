287f1b6899335c53493c299ba6a8df9aa8c4cd8b
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-07 16:17:55 -0600

    feat: update model to include ID of string

--- BooksAPI/Models/VolumeDTO.cs
+++ BooksAPI/Models/VolumeDTO.cs
@@ -3,6 +3,7 @@ namespace BooksAPI.Models
 
   public class VolumeDTO
   {
+    public required string Id { get; set; }
     public required string Title { get; set; }
     public required IList<string> Authors { get; set; }
     public required string Publisher { get; set; }
