b6ddc4d1c5559bc0baa4cf00047b5d436499c720
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-07 16:17:35 -0600

    feat: update context to indicate auto-incremented PK

--- BooksAPI/Models/ReviewContext.cs
+++ BooksAPI/Models/ReviewContext.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace BooksAPI.Models;
 
@@ -9,5 +11,17 @@ public class ReviewContext : DbContext
   {
   }
 
-  public DbSet<Review> Reviews { get; set; } = null!;
+  public DbSet<Review> Review { get; set; } = null!;
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Review>(entity =>
+    {
+      entity.HasKey(e => e.Id); // Set primary key
+      entity.Property(e => e.Id)
+              .ValueGeneratedOnAdd(); // Indicates the ID is auto-incremented
+    });
+
+    base.OnModelCreating(modelBuilder);
+  }
 }
