bde6ae15a6b9d990e1200a0ae845df586747dc7b
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-14 14:01:17 -0600

    feat: add imagelink and infolink, set default values

--- BooksAPI/Models/VolumeDTO.cs
+++ BooksAPI/Models/VolumeDTO.cs
@@ -3,10 +3,12 @@ namespace BooksAPI.Models
 
   public class VolumeDTO
   {
-    public required string Id { get; set; }
-    public required string Title { get; set; }
-    public required IList<string> Authors { get; set; }
-    public required string Publisher { get; set; }
-    public required string Description { get; set; }
+    public string Id { get; set; } = "";
+    public string Title { get; set; } = "";
+    public IList<string> Authors { get; set; } = new List<string>();
+    public string Publisher { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string ImageLink { get; set; } = "";
+    public string InfoLink { get; set; } = "";
   }
 }
